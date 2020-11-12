158960f88f87f9b988a4df4c480fb34b2f59eff9
royvicente90 <r.roy@correo.unimete.edu.ve>
2020-11-11 20:01:50 -0400

    Primer Commit

---
+++ Proyecto1/src/Proyecto1/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Proyecto1;
+
+/**
+ *
+ * @author royvi
+ */
+public class Main {
+    public void main(String [] args){
+        
+        System.out.println("Hello World");
+        
+    }
+    
+}
